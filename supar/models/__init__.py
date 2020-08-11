787d618d31a6112f53cd56bc4bef7da299869758
Matej Klemen <matej.klemen1337@gmail.com>
2020-08-11 10:58:49 +0200

    Remove models that are no longer guaranteed to work/are not the focus v3

--- supar/models/__init__.py
+++ supar/models/__init__.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .constituency import CRFConstituencyModel
-from .dependency import (BiaffineDependencyModel, CRF2oDependencyModel,
-                         CRFDependencyModel, CRFNPDependencyModel)
+from .dependency import BiaffineDependencyModel
 
-__all__ = ['BiaffineDependencyModel',
-           'CRFDependencyModel',
-           'CRF2oDependencyModel',
-           'CRFNPDependencyModel',
-           'CRFConstituencyModel']
+__all__ = ['BiaffineDependencyModel']
